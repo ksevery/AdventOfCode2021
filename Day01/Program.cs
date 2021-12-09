a82167441948e88520aa6a254ec4a82570882518
Konstantin Severi <konstantinsevery@gmail.com>
2021-12-10 00:13:52 +0200

    Add project files.

---
+++ Day01/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+var input = File.ReadAllText("./input.txt");
+
+var lines = input.Split(Environment.NewLine).Select(i => int.Parse(i.Trim())).ToArray();
+var count = 0;
+var prevNum = lines[0];
+for (int i = 1; i < lines.Length; i++)
+{
+    var line = lines[i];
+    if (line > prevNum)
+    {
+        count++;
+        
+    }
+
+    prevNum = line;
+}
+
+Console.WriteLine(count);
+
+count = 0;
+var prevSum = lines[0] + lines[1] + lines[2];
+
+for (int i = 3; i < lines.Length; i++)
+{
+    var sum = lines[i] + lines[i - 1] + lines[i - 2];
+    if (sum > prevSum)
+    {
+        count++;
+    }
+
+    prevSum = sum;
+}
+
+Console.WriteLine(count);
---
+++ Day02/Program.cs
@@ -0,0 +1,80 @@
+﻿
+public class Program
+{
+    public const string Forward = "forward";
+    public const string Down = "down";
+    public const string Up = "up";
+
+
+
+    public static void Main()
+    {
+        var input = File.ReadAllText("./input.txt");
+
+        var horizontalPos = 0;
+        var depth = 0;
+
+        var commandsStr = input.Split(Environment.NewLine).Select(x => x.Trim().Split(new String[] { " " }, StringSplitOptions.RemoveEmptyEntries));
+        var commands = commandsStr.Select(cs => new Command(cs[0], int.Parse(cs[1])));
+
+        foreach (var command in commands)
+        {
+            switch (command.Direction)
+            {
+                case Forward:
+                    horizontalPos += command.HowMuch;
+                    break;
+                case Down:
+                    depth += command.HowMuch;
+                    break;
+                case Up:
+                    depth -= command.HowMuch;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        Console.WriteLine(horizontalPos * depth);
+
+        horizontalPos = 0;
+        depth = 0;
+        var aim = 0;
+
+        foreach (var command in commands)
+        {
+            switch (command.Direction)
+            {
+                case Forward:
+                    horizontalPos += command.HowMuch;
+                    depth += aim * command.HowMuch;
+                    break;
+                case Down:
+                    aim += command.HowMuch;
+                    break;
+                case Up:
+                    aim -= command.HowMuch;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        Console.WriteLine(horizontalPos * depth);
+    }
+}
+
+public class Command
+{
+    public Command(string direction, int howMuch)
+    {
+        Direction = direction;
+        HowMuch = howMuch;
+    }
+
+    public string Direction { get; }
+    public int HowMuch { get; }
+}
+
+
+
