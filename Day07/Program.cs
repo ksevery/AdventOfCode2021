7cd1c7ede504033532b7585d41310cf5010907ef
Konstantin Severi <konstantinsevery@gmail.com>
2021-12-15 00:34:28 +0200

    Day 7, first part of Day 8

---
+++ Day07/Program.cs
@@ -0,0 +1,59 @@
+﻿var input = File.ReadAllText("./input.txt");
+
+var positions = input.Split(",", StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x.Trim()));
+
+var minFuel = positions.Min();
+var maxFuel = positions.Max();
+
+var minFuelSum = int.MaxValue;
+
+for (int i = minFuel; i < maxFuel; i++)
+{
+    var currFuelSum = 0;
+    foreach (var pos in positions)
+    {
+        currFuelSum += Math.Abs(pos - i);
+    }
+
+    if (currFuelSum < minFuelSum)
+    {
+        minFuelSum = currFuelSum;
+    }
+}
+
+Console.WriteLine(minFuelSum);
+
+minFuelSum = int.MaxValue;
+
+for (int i = minFuel; i < maxFuel; i++)
+{
+    var currFuelSum = 0;
+    foreach (var pos in positions)
+    {
+        var totalFuelForMove = 0;
+        var fuelConsumptionIndex = 1;
+        var currPos = pos;
+        while (currPos != i)
+        {
+            totalFuelForMove += fuelConsumptionIndex;
+            fuelConsumptionIndex++;
+            if (currPos < i)
+            {
+                currPos = currPos + 1;
+            }
+            else
+            {
+                currPos = currPos - 1;
+            }
+        }
+
+        currFuelSum += totalFuelForMove;
+    }
+
+    if (currFuelSum < minFuelSum)
+    {
+        minFuelSum = currFuelSum;
+    }
+}
+
+Console.WriteLine(minFuelSum);
---
+++ Day08/Program.cs
@@ -0,0 +1,20 @@
+﻿var input = File.ReadAllText("./input.txt");
+
+var entries = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+var validSymbolCounts = new HashSet<int>()
+{
+    2, 3, 4, 7
+};
+
+var outputValues = entries.Select(e => e.Split("|", StringSplitOptions.RemoveEmptyEntries)[1]);
+
+var validSum = 0;
+
+foreach (var ov in outputValues)
+{
+    var combosCount = ov.Split(" ", StringSplitOptions.RemoveEmptyEntries).Where(c => validSymbolCounts.Contains(c.Length)).Count();
+    validSum += combosCount;
+}
+
+Console.WriteLine(validSum);
